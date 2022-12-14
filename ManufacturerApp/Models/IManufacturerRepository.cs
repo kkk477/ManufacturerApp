20ac440b333c72dbf88a4152d38b1f3c6c3f1b6e
Ray <dlghdnd@gmail.com>
2022-12-14 15:14:48 +0900

    ManufacturerApp 솔루션 생성 및 리포지토리 클래스까지 구현

---
+++ ManufacturerApp/Models/IManufacturerRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace ManufacturerApp.Models
+{
+    public interface IManufacturerRepository
+    {
+        /// <summary>
+        /// 입력
+        /// </summary>
+        /// <param name="manufacturer"></param>
+        /// <returns></returns>
+        Task<Manufacturer> AddManufacturer(Manufacturer manufacturer);
+        /// <summary>
+        /// 출력
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Manufacturer>> GetManufacturers();
+        /// <summary>
+        /// 상세
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Manufacturer> GetManufacturer(int id);
+        /// <summary>
+        /// 수정
+        /// </summary>
+        /// <param name="manufacturer"></param>
+        /// <returns></returns>
+        Task<Manufacturer> EditManufacturer(Manufacturer manufacturer);
+        /// <summary>
+        /// 삭제
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task DeleteManufacturer(int id);
+    }
+}
---
+++ ManufacturerApp/Models/ManufacturerRepository.cs
@@ -0,0 +1,73 @@
+﻿using ManufacturerApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManufacturerApp.Models
+{
+    public class ManufacturerRepository : IManufacturerRepository
+    {
+        ApplicationDbContext _context;
+        public ManufacturerRepository(ApplicationDbContext context)
+        {
+            _context= context;
+        }
+
+        /// <summary>
+        /// 입력
+        /// </summary>
+        /// <param name="manufacturer"></param>
+        /// <returns></returns>
+        public async Task<Manufacturer> AddManufacturer(Manufacturer manufacturer)
+        {
+            _context.Add(manufacturer);
+            await _context.SaveChangesAsync();
+            return manufacturer;
+        }
+        /// <summary>
+        /// 출력
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Manufacturer>> GetManufacturers()
+        {
+            return await _context.Manufacturers.OrderBy(m => m.Id).ToListAsync();
+        }
+        /// <summary>
+        /// 상세
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Manufacturer> GetManufacturer(int id)
+        {
+            return await _context.Manufacturers.Where(m => m.Id == id).SingleOrDefaultAsync();
+        }
+        /// <summary>
+        /// 수정
+        /// </summary>
+        /// <param name="manufacturer"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<Manufacturer> EditManufacturer(Manufacturer manufacturer)
+        {
+            _context.Entry(manufacturer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return manufacturer;
+        }
+        /// <summary>
+        /// 삭제
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task DeleteManufacturer(int id)
+        {
+            var manufacturer = await _context.Manufacturers.Where(m => m.Id == id).SingleOrDefaultAsync();
+            if (manufacturer != null)
+            {
+                _context.Manufacturers.Remove(manufacturer);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+
+
+    }
+}
